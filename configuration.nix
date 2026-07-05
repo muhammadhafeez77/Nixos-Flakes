ca6d80b56fa6525d813bc1107b0dd5d48947453d
muhammadhafeez77 <246017655+muhammadhafeez77@users.noreply.github.com>
2026-07-05 06:44:20 +0500

    Actually Adding this time :/

--- configuration.nix
+++ configuration.nix
@@ -1,1 +1,200 @@
-/etc/nixos/configuration.nix
+# Edit this configuration file to define what should be installed on
+# your system. Help is available in the configuration.nix(5) man page, on
+# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
+
+{ config, lib, pkgs, ... }:
+
+let 
+  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-26.05.tar.gz";
+in
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+	(import "${home-manager}/nixos")
+    ];
+
+  # Home Manager
+  home-manager.useUserPackages = true;
+  home-manager.useGlobalPkgs = true;
+  home-manager.backupFileExtension = "backup";
+  home-manager.users.coffee = import ./home.nix;
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # Use latest kernel.
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+
+  networking.hostName = "nixos-btw"; # Define your hostname.
+
+  # Configure network connections interactively with nmcli or nmtui.
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Asia/Karachi";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkb.options in tty.
+  # };
+
+  # Enable the X11 windowing system.
+   
+  services.xserver = {
+	enable=true;
+	windowManager.qtile.enable = true;
+	displayManager.sessionCommands = ''
+	xset r rate 200 35 &
+	'';
+ };
+  
+  
+
+  # Configure keymap in X11
+  # services.xserver.xkb.layout = "us";
+  # services.xserver.xkb.options = "eurosign:e,caps:escape";
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # services.pulseaudio.enable = true;
+  # OR
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+   users.users.coffee = {
+     isNormalUser = true;
+     extraGroups = [ "wheel" ]; # Enable ‘sudo’ for the user.
+     packages = with pkgs; [
+       tree
+     ];
+   };
+
+   programs.firefox.enable = true;
+
+  # List packages installed in system profile.
+  # To search for packages , run ,  nix search wget
+  # You can use https://search.nixos.org/ to find more packages (and options).
+  environment.systemPackages = with pkgs; [
+        vim-full # Do not forget to add an editor to edit configuration.nix!
+	neovim
+	alacritty
+        wget
+   	fastfetch
+	htop
+	btop
+	sl
+	cmatrix
+	cbonsai
+	git 
+	tealdeer
+	xclip 
+	maim	
+	slop
+	bat 
+	tree
+	fzf
+	killall
+	ranger
+	thunar	 
+	xwallpaper
+	rofi
+	picom
+	mousepad
+	unzip
+	python3
+	gcc
+	
+   ];
+
+  # Add Fonts
+  fonts.packages = with pkgs; [
+ 	jetbrains-mono
+   ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+	services.openssh = {
+  	enable = true;
+  	settings = {
+    	X11Forwarding = false;
+    		PermitRootLogin = "yes"; 
+    		PasswordAuthentication = true; 
+  };
+};
+
+  #Set FZF as Reverse search in terminal
+  programs.bash.interactiveShellInit = ''
+  source ${pkgs.fzf}/share/fzf/completion.bash
+  source ${pkgs.fzf}/share/fzf/key-bindings.bash
+    '';
+
+  # So that sudo vim also yanks properly 
+  security.sudo.extraConfig = ''
+     Defaults env_keep += "DISPLAY XAUTHORITY"
+    '';
+
+  #Enable Pivom for transparency 
+  services.picom = {
+	enable = true;
+	backend = "glx";
+	fade = true;
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This option defines the first version of NixOS you have installed on this particular machine,
+  # and is used to maintain compatibility with application data (e.g. databases) created on older NixOS versions.
+  #
+  # Most users should NEVER change this value after the initial install, for any reason,
+  # even if you've upgraded your system to a new NixOS release.
+  #
+  # This value does NOT affect the Nixpkgs version your packages and OS are pulled from,
+  # so changing it will NOT upgrade your system - see https://nixos.org/manual/nixos/stable/#sec-upgrading for how
+  # to actually do that.
+  #
+  # This value being lower than the current NixOS release does NOT mean your system is
+  # out of date, out of support, or vulnerable.
+  #
+  # Do NOT change this value unless you have manually inspected all the changes it would make to your configuration,
+  # and migrated your data accordingly.
+  #
+  # For more information, see `man configuration.nix` or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
+  system.stateVersion = "26.05"; # Did you read the comment?
+
+}
+
--- home.nix
+++ home.nix
@@ -1,1 +1,55 @@
-/etc/nixos/home.nix
+{ config, pkgs, ... }:
+
+{
+  home.username = "coffee";
+  home.homeDirectory = "/home/coffee";
+  home.stateVersion = "26.05";
+
+  programs.bash = {
+    enable = true;
+    shellAliases = {
+      btw = "echo i use nixos btw";
+      nrs = "sudo nixos-rebuild switch";
+    };
+
+    initExtra = ''
+      export PS1='\[\e[38;5;76m\]\u\[\e[0m\] in \[\e[38;5;32m\]\w\[\e[0m\] \\$ '
+  
+    '';                              
+  };
+
+
+ programs.vim = {                       #### VIM 
+   enable = true;
+   extraConfig = ''
+        set clipboard=unnamedplus
+   '';
+ };
+
+programs.alacritty ={            #### ALACRITTY
+  enable = true;
+  settings = {
+    window = {
+      opacity = 0.96;
+      padding = { y = 6; x = 6; };
+    };
+    font = {
+      size = 8;
+      normal = {
+        family = "JetBrains Mono";
+        style = "Regular";
+      };
+    };
+    keyboard.bindings = [
+      { key = "V"; mods = "Control"; action = "Paste"; }
+      { key = "C"; mods = "Control|Shift"; action = "Copy"; }
+    ];
+  };
+};
+#  home.file.".config/qtile".source = /home/tony/home-manager-dotfiles/qtile;
+
+  home.packages = with pkgs; [
+    bat
+  ];
+}
+
