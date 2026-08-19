f4a5cbbce2c6a5ca4cbd819fe384536b20095c3e
Death Angel <muhammadhafeez7397@gmail.com>
2026-08-19 12:18:02 +0500

    Cleaning flake.nix

--- flake.nix
+++ flake.nix
@@ -15,7 +15,6 @@
     noctalia = {
       url = "github:noctalia-dev/noctalia-shell";
       inputs.nixpkgs.follows = "nixpkgs";
-      #      inputs.quickshell.follows = "quickshell"; # Use same quickshell version
     };
   };
 
@@ -42,30 +41,3 @@
       };
     };
 }
-#{
-#  description = "Trying to make NixOS daily drivable";
-#  inputs = {
-#    nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
-#    home-manager.url = "github:nix-community/home-manager/release-26.05";
-#    home-manager.inputs.nixpkgs.follows = "nixpkgs";
-#  };
-#  outputs = { self, nixpkgs, home-manager, ... }:
-#    let
-#      system = "x86_64-linux";
-#    in
-#    {
-#      nixosConfigurations.nixos-btw = nixpkgs.lib.nixosSystem {
-#        inherit system;
-#        modules = [
-#          ./configuration.nix
-#          home-manager.nixosModules.home-manager
-#          {
-#            home-manager.useGlobalPkgs = true;
-#            home-manager.useUserPackages = true;
-#            home-manager.users.coffee = import ./home.nix;
-#            home-manager.backupFileExtension = "backup";
-#          }
-#        ];
-#      };
-#    };
-#}
